130827d2b40356e4313441b8b833da823d1fefa1
Gustavo Silva <gustavo.silva@ear.local>
2022-10-05 21:15:55 -0300

    ok_hand: style: put add flow form to apper

--- src/components/AddFlowForm.tsx
+++ src/components/AddFlowForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/components/AddFlowForm.scss';
 
@@ -8,43 +8,94 @@ type AddFlowForm = {
 }
 
 function AddFlowForm({ isOpened, setIsOpened }: AddFlowForm) {
+    const [isShowingInFlow, setIsShowingInFlow] = useState(true);
+
     return (
         <div className={`add-flow-form ${isOpened ? ("isOpened") : ("")}`}>
-            <span className="title add-flow-title">Novo fluxo de caixas</span>
-            <div className="kind-of-flow">
-                <select id="kind-of-flow">
-                    <option value="0">Receita</option>
-                    <option value="1">Despesa</option>
-                </select>
-            </div>
+
             <form action="submit">
                 <div className="form-group">
                     <div className="form-item">
-                        <label>Nome</label>
-                        <input value="Ana Luiza" />
-                    </div>
-                </div>
-                <div className="form-group">
-                    <div className="form-item">
-                        <label>Lado</label>
-                        <select>
-                            <option>A</option>
-                            <option>B</option>
+                        <label>Novo fluxo de caixa</label>
+                        <select id="kind-of-flow" onChange={() => { setIsShowingInFlow(!isShowingInFlow) }}>
+                            <option value="0">Receita</option>
+                            <option value="1">Despesa</option>
                         </select>
                     </div>
+                </div>
+                {
+                    isShowingInFlow ? (
+                        <InFlow />
+                    ) : (
+                        <OutFlow />
+                    )
+                }
 
-                    <div className="form-item">
-                        <label>Apt</label>
-                        <select>
-                            <option>Apt. 102</option>
-                            <option>Apt. 103</option>
-                        </select>
-                    </div>
-                </div>
                 <div className="form-item-submit">
                     <input type="submit" value="Registrar" />
                 </div>
             </form>
+        </div>
+    )
+}
+
+
+function InFlow() {
+    return (
+        <div className="form-flow-items">
+            <div className="form-group">
+                <div className="form-item">
+                    <label>Descrição</label>
+                    <input value="Pintura das paredes" />
+                </div>
+            </div>
+            <div className="form-group">
+                <div className="form-item">
+                    <label>Lado</label>
+                    <select>
+                        <option>A</option>
+                        <option>B</option>
+                    </select>
+                </div>
+
+                <div className="form-item">
+                    <label>Apt</label>
+                    <select>
+                        <option>Apt. 102</option>
+                        <option>Apt. 103</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function OutFlow() {
+    return (
+        <div className="form-flow-items">
+            <div className="form-group">
+                <div className="form-item">
+                    <label>Nome</label>
+                    <input value="Ana Luiza" />
+                </div>
+            </div>
+            <div className="form-group">
+                <div className="form-item">
+                    <label>Lado</label>
+                    <select>
+                        <option>A</option>
+                        <option>B</option>
+                    </select>
+                </div>
+
+                <div className="form-item">
+                    <label>Apt</label>
+                    <select>
+                        <option>Apt. 102</option>
+                        <option>Apt. 103</option>
+                    </select>
+                </div>
+            </div>
         </div>
     )
 }
